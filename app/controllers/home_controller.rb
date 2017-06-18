8cf7e68edacbc8df9687a80f52c13289982351e1
Duy Phuc <phuc.tran@yomi.studio>
2017-06-18 14:28:34 +0700

    User can sort menu items by “alphabetical”, “price low to high”, and “price high to low”.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -7,11 +7,20 @@ class HomeController < ApplicationController
 
   def menu
     @sections  = Section.all
+    @sort_type = 0
     if params[:section_id]
       @section = @sections.find params[:section_id].to_i
       @food_items = @section.food_items
     elsif
       @food_items = FoodItem.all
+    end
+    @sort_type = params[:sort_type] ? params[:sort_type].to_i : 0
+    if @sort_type == 0
+      @food_items = @food_items.order('name ASC')
+    elsif @sort_type == 1
+      @food_items = @food_items.order('price ASC')
+    elsif @sort_type == 2
+      @food_items = @food_items.order('price DESC')
     end
   end
 end
